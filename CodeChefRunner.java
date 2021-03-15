0006aeae1585f929fbdec578928980d0fa25a7e8
Nitin Jayant <thenitinjayant@gmail.com>
2021-03-15 20:04:30 +0530

    runner added

--- CodeChefRunner.java
+++ CodeChefRunner.java
@@ -2,7 +2,6 @@ package GFG_MDI_Questions;
 
 public class CodeChefRunner {
     public void run(){
-        MissingNumberInArray missingNumberInArray = new MissingNumberInArray();
-        missingNumberInArray.mainMissingNumberInArray();
+
     }
 }
